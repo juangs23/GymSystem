58c79497b11c55d20a3732e3fae0a7eec033d0f3
juangs23 <juangymsystem@gmail.com>
2023-11-15 08:22:14 -0500

    Nuevas actualiazaciones login

--- hbs/index.js
+++ hbs/index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const path = require('path') //Trabajar con rutas
 const hbs = require('hbs') //Incorporar motor de plantillas
 
+const formArray = [];
 const app = express()
 const port = 8080
+
+// Configuración del middleware para analizar datos POST
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'))
 
@@ -21,13 +25,80 @@ app.get('/', (req, res)=>{
     })//Redireccionar hacia el archivo hbs
 })
 
-app.get('/sign-in', (req, res)=>{
-    res.render('sign-in')
+// Inicio Gestion de acceso
+app.get('/ingresar', (req, res)=>{
+    res.render('auth/sign-in')
 })
 
-app.get('/rutinas', (req, res)=>{
-    res.render('rutinas')
+app.get('/registrar-usuario', (req, res)=>{
+    res.render('auth/sign-up')
 })
+
+app.get('/recuperar-contrasena', (req, res)=>{
+    res.render('auth/recoverpw')
+})
+
+app.get('/confirmacion', (req, res)=>{
+    res.render('auth/confirm-mail')
+})
+
+app.get('/perfil', (req, res)=>{
+    res.render('auth/user-account-setting')
+})
+
+app.get('/restablecer', (req, res)=>{
+    res.render('auth/new-password')
+})
+
+// Fin Gestion de acceso
+
+// Inicio Ventas
+
+app.get('/clientes', (req, res)=>{
+    res.render('clientes')
+})
+app.get('/beneficiarios', (req, res)=>{
+    res.render('beneficiarios')
+})
+app.get('/formularioCliente', (req, res)=>{
+    res.render('formularioCliente')
+})
+app.get('/formularioBeneficiario', (req, res)=>{
+    res.render('formularioBeneficiario')
+})
+app.get('/formularioPedido', (req, res)=>{
+    res.render('formularioPedido')
+})
+app.get('/pedidos', (req, res)=>{
+    res.render('pedidos')
+})
+app.get('/ventas', (req, res)=>{
+    res.render('ventas')
+})
+app.get('/devoluciones', (req, res)=>{
+    res.render('devoluciones')
+})
+
+//Fin Ventas
+
+//Rutinas
+app.get('/rutinas', (req, res)=>{
+    res.render('rutinas', { datos: formArray});
+});
+
+app.get('/nueva-rutina', (req, res)=>{
+    res.render('formRutina')
+});
+
+app.post('/guardar-rutinas', (req, res)=>{
+    const { nombreRutina, dia, exercise1, repeticion } = req.body;
+
+    formArray.push ({ nombreRutina, dia, exercise1, repeticion });
+
+    res.redirect('/rutinas');
+});
+
+// Fin rutinas
 
 app.get('/plantilla', (req, res)=>{
     res.render('plantilla')
@@ -36,8 +107,6 @@ app.get('/plantilla', (req, res)=>{
 app.get('*', (req, res)=>{
     res.render('404')
 })
-
-
 
 app.listen(port, () => {
     console.log(`Escuchado por el puerto ${port}`)
