20e1ecb42f6079bc577c47391f5aab5590d82fc3
DorlixitoG <vbkevin.iesanpablo@gmail.com>
2023-11-27 09:35:45 -0500

    actualizacion modal1

--- hbs/public/assets/js/productos.js
+++ hbs/public/assets/js/productos.js
@@ -96,6 +96,21 @@ formularioproductos.addEventListener('submit',(e) => {
         });
     } else {
         alert("malo")
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "error",
+            title: "Ingrese los datos correctamente"
+          });
         document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo')
         setTimeout(() => {
             document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
