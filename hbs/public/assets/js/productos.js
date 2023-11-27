a8fca4954f90057e6e29adcabbbfb6affd063929
DorlixitoG <vbkevin.iesanpablo@gmail.com>
2023-11-27 09:12:29 -0500

    actualizacion modal1

--- hbs/public/assets/js/productos.js
+++ hbs/public/assets/js/productos.js
@@ -72,22 +72,20 @@ formularioproductos.addEventListener('submit',(e) => {
     e.preventDefault();
     if(campos.Nombreproducto && campos.Precioproducto && campos.Productosingresar && campos.Ivaproducto){
         formularioproductos.reset()
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-          }).then((result) => {
-            if (result.isConfirmed) {
-              Swal.fire({
-                title: "Deleted!",
-                text: "Your file has been deleted.",
-                icon: "success"
-              });
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
             }
+          });
+          Toast.fire({
+            icon: "success",
+            title: "Guardado correctamente"
           });
         document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
         setTimeout(() => {
