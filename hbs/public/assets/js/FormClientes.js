663949b45590da821375ac7232ee557155b172cc
alejogymsystem <150259708+alejogymsystem@users.noreply.github.com>
2023-11-29 06:16:39 -0500

    Revert "Alejo builes"

--- hbs/public/assets/js/FormClientes.js
+++ hbs/public/assets/js/FormClientes.js
@@ -1,123 +1,74 @@
-const formularioRegistro = document.getElementById('formularioRegistro')
-const inputs = document.querySelectorAll('#formularioRegistro input')
+const formulario = document.getElementById('FormularioCliente');
 
-const expresiones = {
-	nombres: /^[a-zA-ZÀ-ÿ\s]{1,20}$/, // Letras y espacios, pueden llevar acentos.
-    apellidos: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
-    documento: /^[A-Za-z0-9\s-]{10}$/,
-	password:  /^(?=.*[A-Z])(?=.*\d).+$/, // 4 a 12 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    edad: /^(1[8-9]|[2-5][0-9]|6[0-5])$/,
-    direccion: /^[A-Za-z0-9\s#áéíóúÁÉÍÓÚüÜ.,-]+$/,
-	telefono: /^(\(\+?\d{2,3}\)[\*|\s|\-|\.]?(([\d][\*|\s|\-|\.]?){6})(([\d][\s|\-|\.]?){2})?|(\+?[\d][\s|\-|\.]?){8}(([\d][\s|\-|\.]?){2}(([\d][\s|\-|\.]?){2})?)?)$/
+if (formulario) {
+    formulario.addEventListener("submit", function (event) {
+        event.preventDefault();
+        validateForm();
+    });
 }
 
-const campos = {
-    nombres: false, 
-    apellidos: false, 
-    documento: false, 
-    correo: false, 
-    telefono: false, 
-    edad: false,
-    direccion: false,
-    password: false
-}
-
-const validarFormulario = (e) => {
-    switch (e.target.name) {
-        case "nombres":
-            validarCampo(expresiones.nombres, e.target, 'nombres')
-        break
-        case "apellidos":
-            validarCampo(expresiones.apellidos, e.target, 'apellidos')
-        break
-        case "documento":
-            validarCampo(expresiones.documento, e.target, 'documento')
-        break
-        case "correo":
-            validarCampo(expresiones.correo, e.target, 'correo')
-        break
-        case "telefono":
-            validarCampo(expresiones.telefono, e.target, 'telefono')
-        break
-        case "edad":
-            validarCampo(expresiones.edad, e.target, 'edad')
-        break
-        case "direccion":
-            validarCampo(expresiones.direccion, e.target, 'direccion')
-        break
-        case "password":
-            validarCampo(expresiones.password, e.target, 'password')
-            validarPassword2()
-        break
-        case "confirmarPassword":
-            validarPassword2()
-        break
+document.addEventListener("DOMContentLoaded", function () {
+    function validateNombre(input) {
+        const regex = /^[a-zA-Z\s]*$/;
+        return regex.test(input.value);
     }
-}
 
-const validarCampo = (expresion, input, campo) => {
-    if (expresion.test(input.value)) {
-        document.getElementById(`grupo__${campo}`).classList.remove('formularioRegistro__grupo-incorrecto')
-        document.getElementById(`grupo__${campo}`).classList.add('formularioRegistro__grupo-correcto')
-        document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle')
-        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle')
-        document.querySelector(`#grupo__${campo} .formularioRegistro__input-error`).classList.remove('formularioRegistro__input-error-activo')
-        campos[campo] = true
-    }else {
-        document.getElementById(`grupo__${campo}`).classList.add('formularioRegistro__grupo-incorrecto')
-        document.getElementById(`grupo__${campo}`).classList.remove('formularioRegistro__grupo-correcto')
-        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle')
-        document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle')
-        document.querySelector(`#grupo__${campo} .formularioRegistro__input-error`).classList.add('formularioRegistro__input-error-activo')
-        campo[false]
+    function validateDocumento(input) {
+        const regex = /^\d{10}$/;
+        return regex.test(input.value);
     }
-}
 
-const validarPassword2 = () =>{
-    const inputPassword1 = document.getElementById('password')
-    const inputPassword2 = document.getElementById('confirmarPassword')
-
-    if (inputPassword1.value !== inputPassword2.value) {
-        document.getElementById(`grupo__confirmarPassword`).classList.add('formularioRegistro__grupo-incorrecto')
-        document.getElementById(`grupo__confirmarPassword`).classList.remove('formularioRegistro__grupo-correcto')
-        document.querySelector(`#grupo__confirmarPassword i`).classList.remove('fa-check-circle')
-        document.querySelector(`#grupo__confirmarPassword i`).classList.add('fa-times-circle')
-        document.querySelector(`#grupo__confirmarPassword .formularioRegistro__input-error`).classList.add('formularioRegistro__input-error-activo')
-        campos['password'] = false
-    } else {
-        document.getElementById(`grupo__confirmarPassword`).classList.remove('formularioRegistro__grupo-incorrecto')
-        document.getElementById(`grupo__confirmarPassword`).classList.add('formularioRegistro__grupo-correcto')
-        document.querySelector(`#grupo__confirmarPassword i`).classList.add('fa-check-circle')
-        document.querySelector(`#grupo__confirmarPassword i`).classList.remove('fa-times-circle')
-        document.querySelector(`#grupo__confirmarPassword .formularioRegistro__input-error`).classList.remove('formularioRegistro__input-error-activo')
-        campos['password'] = true
+    function validateCorreo(input) {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(input.value);
     }
-}
 
-inputs.forEach((input) => {
-    input.addEventListener('keyup', validarFormulario)
-    input.addEventListener('blur', validarFormulario)
-})
+    function validateDireccion(input) {
+        const regex = /^[a-zA-Z0-9\s]*$/;
+        return regex.test(input.value);
+    }
 
-formularioRegistro.addEventListener('submit', (e) =>{
-    e.preventDefault();
+    function validateContacto(input) {
+        const regex = /^[a-zA-Z\s]*$/;
+        return regex.test(input.value);
+    }
 
-    const terminos = document.getElementById('terminos')
-    if (campos.nombres && campos.apellidos && campos.documento && campos.correo && campos.edad && campos.telefono && 
-        campos.direccion && campos.password && terminos.checked) {
-        formularioRegistro.reset()
+    function updateErrorMessage(id, isValid, message) {
+        const errorSpan = document.getElementById(id);
+        errorSpan.textContent = isValid ? '' : message;
+    }
 
-        document.getElementById('formularioRegistro__mensaje-exito').classList.add('formularioRegistro__mensaje-exito-activo');
-        setTimeout(() =>{
-            document.getElementById('formularioRegistro__mensaje-exito').classList.remove('formularioRegistro__mensaje-exito-activo');
-        }, 3000)
-        document.querySelectorAll('.formularioRegistro__grupo-correcto').forEach((icono) => {
-            icono.classList.remove('formularioRegistro__grupo-correcto')
-        })
+    function validateForm() {
+        const nombreInput = document.querySelector('input[name="NombreCliente"]');
+        const tipoDocumentoInput = document.querySelector('select[name="tipoDocumento"]');
+        const documentoInput = document.querySelector('input[name="DocumentoCliente"]');
+        const correoInput = document.querySelector('input[name="correoCliente"]');
+        const direccionInput = document.querySelector('input[name="direccionCliente"]');
+        const contactoInput = document.querySelector('input[name="contactoCliente"]');
 
-    } else {
-        errorElement.textContent = '';
+        const isValidNombre = validateNombre(nombreInput);
+        const isValidTipoDocumento = tipoDocumentoInput.value !== "";
+        const isValidDocumento = validateDocumento(documentoInput);
+        const isValidCorreo = validateCorreo(correoInput);
+        const isValidDireccion = validateDireccion(direccionInput);
+        const isValidContacto = validateContacto(contactoInput);
+
+        updateErrorMessage('nombre-error', isValidNombre, 'Solo se permiten letras y espacios en el nombre');
+        updateErrorMessage('tipoDocumento-error', isValidTipoDocumento, 'Seleccione un tipo de documento');
+        updateErrorMessage('DocumentoCliente-error', isValidDocumento, 'Ingrese un número de documento válido (10 dígitos)');
+        updateErrorMessage('correo-error', isValidCorreo, 'Ingrese una dirección de correo electrónico válida');
+        updateErrorMessage('direccion-error', isValidDireccion, 'Solo se permiten letras, números y espacios en la dirección');
+        updateErrorMessage('contacto-error', isValidContacto, 'Solo se permiten letras y espacios en el contacto');
+
+        if (isValidNombre && isValidTipoDocumento && isValidDocumento && isValidCorreo && isValidDireccion && isValidContacto) {
+            alert('Formulario válido. Guardar información.');
+        } else {
+            alert('El formulario no es válido. Por favor, revisa los campos.');
+        }
+    }
+
+    const guardarButton = document.querySelector('button[name="Guardar"]');
+    if (guardarButton) {
+        guardarButton.addEventListener("click", validateForm);
     }
 });
-
